0d7211d0f0e4f32aef1fd8b59e920ce620b40bb8
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-07-20 23:05:31 +0200

    passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    if (numberOfTimes<0){
+        return "ERROR";
+    } else {
+    let repeatedString = '';
+    for (let i=0; i<numberOfTimes; i++){
+        repeatedString += stringToRepeat;
+    };
+    return repeatedString;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    for (let index = 0; index<string.length; index ++){
+        reversed += string[string.length - index -1];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
