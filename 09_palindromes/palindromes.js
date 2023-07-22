0d9d5d00e2b2aea12ad7b2a758a153b3bb0255a7
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-07-22 19:40:29 +0200

    Passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reversedString = string.split('').reverse().join('');
+    let noSpaceString = string.replace(/[^\w]|_/g, '');
+    let noSpaceReversedString = reversedString.replace(/[^\w]|_/g, '');
+
+    if (noSpaceReversedString.toLowerCase() == noSpaceString.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestIndex = 0;
+    for (let index = 1; index < people.length; index ++){
+        if (age(people[oldestIndex]) < age(people[index])){
+            oldestIndex = index;
+        }
+    }
+    return people[oldestIndex];
 };
+
+function age(person){
+    let age;
+    let isAlive = person.yearOfDeath == undefined;
+    if (isAlive){
+        age = (new Date()).getFullYear() - person.yearOfBirth;
+    } else {
+        age = person.yearOfDeath - person.yearOfBirth;
+    }
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
