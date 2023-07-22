e137e2f556e2f2e031bd3e206656d2fa9b7c54e1
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-07-22 15:31:48 +0200

    Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ( a<0 || b<0){
+        return "ERROR";
+    };
 
+    let types = ['number', 'bigint'];
+    if (!types.includes(typeof(a)) || !types.includes(typeof(b))){
+        return "ERROR";
+    };
+
+    let first, last;
+    
+    if (a<=b){
+        first = a;
+        last = b;
+    } else {
+        first = b;
+        last = a;
+    }
+    let sum = 0;
+    for (let j = first; j <= last; j++){
+        sum += j;
+    };
+    return sum;
 };
 
 // Do not edit below this line
